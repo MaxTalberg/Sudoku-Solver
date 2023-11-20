76ecf4619111fb01870163a09fc7f10b0d4a73f3
Max Talberg <mt942@cam.ac.uk>
2023-11-20 17:04:03 +0000

    Initial commit

---
+++ src/suduko_solver.py
@@ -0,0 +1,11 @@
+'''
+PROTOTYPING
+Read the Sudoku puzzle from input or define it as a 2D array.
+Implement a function to check if a number is valid in a given row.
+Implement a function to check if a number is valid in a given column.
+Implement a function to check if a number is valid in a given 3x3 subgrid.
+Implement a function to check if a number is valid at a given position in the Sudoku grid.
+Implement a function to find an empty position in the Sudoku grid.
+Implement a recursive function to solve the Sudoku puzzle using backtracking.
+Print the solved Sudoku puzzle.
+'''
