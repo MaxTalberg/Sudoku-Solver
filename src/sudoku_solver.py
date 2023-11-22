c9300dec40177e1ab4906b2d1109798df1b50e88
Max Talberg <mt942@cam.ac.uk>
2023-11-22 15:13:16 +0000

    Created a class for all functions and added get_board function
    Class encapsulates sudoku solving functions organising the code and allowing easy extension of algorithms, testing and debugging

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -1,149 +1,170 @@
 '''
 PROTOTYPING
-Read the Sudoku puzzle from input or define it as a 2D array.
+
+read_board_from_file(filename):
 Unit testing
 - Test with non-existent file
 - Test with badly formatted file
 - Test with correct file
 
 
-Implement a function to check if a number is valid:
+check_possible_indicies(board, x, y, n):
 Unit testing
 - Test with true values
 - Test with false values columnn and row and 3x3 square
 - Test edge cases
 
-Implement a recursive function to solve the Sudoku puzzle using backtracking
+solve_sudoku(board):
+- Test solvable board
+- Test solved board
+- Test unsolvable board
+- Test empty board
+- Test invalid board size
 based on the validity of the function above.
-
 
 '''
 import sys
 import numpy as np
 
+class SudokuSolver:
+    '''Class to solve a sudoku'''
 
-def read_board_from_file(filename):
-    '''
-    Reads a sudoku puzzle from a text file
-    and converts it into a list of lists where 
-    each sublist represents a row of the sudoku
-
-    Parameters
-    ----------
-    filename: str
-        numbers for the initial sudoku board
+    # initialise board from file
+    def __init__(self, filename):
+        self.board = self.read_board_from_file(filename)
     
-    Returns
-    ----------
-    list(int)
-        A list of lists representing each row
-        of the initial sudoko board
+    def read_board_from_file(self, filename):
+        '''
+        Reads a sudoku puzzle from a text file
+        and converts it into a list of lists where 
+        each sublist represents a row of the sudoku
 
-    Add error trapping ---> unit tests
-    '''
-    # Initialise the empty board
-    board = []
+        Parameters
+        ----------
+        filename: str
+            numbers for the initial sudoku board
+    
+        Returns
+        ----------
+        list(int)
+            A list of lists representing each row
+            of the initial sudoko board
 
-    with open(filename, 'r') as file:
-        for line in file:
+        Add error trapping ---> unit tests
+        '''
 
-            # Ignore the grid separators
-            if line.startswith("---"):
-                continue
+        # Initialise the empty board
+        board = []
 
-            # Remove the grid dividers and newline characters, then split into numbers
-            row = line.replace('|', '').replace('\n', '')
+        with open(filename, 'r') as file:
+            for line in file:
 
-            # Convert each character to an integer
-            board.append([int(char) for char in row])
+                # Ignore the grid separators
+                if line.startswith("---"):
+                    continue
 
-    # Return the matrix of the inserted sudoku board
-    return board
+                # Remove the grid dividers and newline characters, then split into numbers
+                row = line.replace('|', '').replace('\n', '')
 
+                # Convert each character to an integer
+                board.append([int(char) for char in row])
 
-def check_possible_indicies(board, x, y, n):
+        # Return the matrix of the inserted sudoku board
+        return board
 
-    '''
-    Checks if a certain indicies is possible
-    on a certain grid of the sudoku board
+    def check_possible_indicies(self, x, y, n):
+        '''
+        Checks if a certain indicies is possible
+        on a certain grid of the sudoku board
 
-    Parameters
-    ----------
-    board: list(list(int))
-        The current state of the sudoku board
+        Parameters
+        ----------
+        board: list(list(int))
+            The current state of the sudoku board
 
-    x: int
-        Column number of the sudoku board
+        x: int
+            Column number of the sudoku board
 
-    y: int
-        Row number of the sudoku board
+        y: int
+            Row number of the sudoku board
 
-    n: int
-        indicie to check in the grid square
+        n: int
+            indicie to check in the grid square
 
-    Returns
-    ----------
-    Boolean
-        True if number is allowed, False otherwise
+        Returns
+        ----------
+        Boolean
+            True if number is allowed, False otherwise
+        '''
 
-    '''
-
-    # checks if n is in column x
-    for i in range(0,9):
-        if board[i][x] == n:
-            #print('x: ', [x+1, i+1])
-            return False
-        
-    # checks if n is in row y
-    for i in range(0,9):
-        if board[y][i] == n:
-            #print('y: ' ,[i+1, y+1])
-            return False
-        
-    # top left corner of 3x3 square
-    x0 = (x // 3) * 3
-    y0 = (y // 3) * 3
-
-    # checks if n is in 3x3 square
-    for i in range(0,3):
-        for j in range(0,3):
-            if board[y0 + i][x0 + j] == n:
+        # checks if n is in column x
+        for i in range(0,9):
+            if self.board[i][x] == n:
                 return False
+        
+        # checks if n is in row y
+        for i in range(0,9):
+            if self.board[y][i] == n:
+                return False
+        
+        # top left corner of 3x3 square
+        x0 = (x // 3) * 3
+        y0 = (y // 3) * 3
+
+        # checks if n is in 3x3 square
+        for i in range(0,3):
+            for j in range(0,3):
+                if self.board[y0 + i][x0 + j] == n:
+                    return False
             
-    # return True if intiger is allowed on square x, y
-    return True
+        # return True if intiger is allowed on square x, y
+        return True
 
-def solve_sudoku(board):
-    '''
-    Solves the sudoku by recurssion of
-    possbile indicies
+    def solve_sudoku(self):
+        '''
+        Solves the sudoku by recurssion of
+        possbile indicies
 
-    Parameters
-    ----------
-    board: list(list(int))
-        The current state of the sudoku board
+        Parameters
+        ----------
+        board: list(list(int))
+            The current state of the sudoku board
 
-    Returns
-    ----------
-    Matrix
-        Matrix of completed sudoku board!
+        Returns
+        ----------
+        board: list(list(int))
+            The solved sudoku board
 
-    '''
-    # checks if square is blank, contains a 0
-    for x in range(0,9):
-        for y in range(0,9):
-            if board[y][x] == 0:
+        '''
+        # checks if square is blank, contains a 0
+        for x in range(0,9):
+            for y in range(0,9):
+                if self.board[y][x] == 0:
 
-                # check if number n can be here
-                for n in range(1,10):
-                    if check_possible_indicies(board, x,y,n):
-                        board[y][x] = n
-                        if solve_sudoku(board):
-                            return board
-                        # backtracking!
-                        board[y][x] = 0
-                return None
-    return board
+                    # check if number n can be here
+                    for n in range(1,10):
+                        if self.check_possible_indicies(x,y,n):
+                            self.board[y][x] = n
+                            if self.solve_sudoku():
+                                return self.board
+                            # backtracking!
+                            self.board[y][x] = 0
+                    return None
+        # returns board when a solution is found
+        return self.board
+    
+    def get_board(self):
+        '''
+        Provides current state of the sudoku board
+
+        Returns
+        ----------
+        board: list(list(int))
+            The current state of the sudoku board
+
+        '''
+        return self.board
+
 def main():
 
     # Check the correct number of command line arguements are passsed
@@ -155,13 +176,12 @@ def main():
     input_file = sys.argv[1]
 
     # initial unsolved sudoku
-    board = read_board_from_file(input_file)
-    print('\nInitial unsolved sudoku: \n\n', np.matrix(board))
+    sododku_solver = SudokuSolver(input_file)
+    print('\nInitial unsolved sudoku: \n\n', np.matrix(sododku_solver.get_board()))
 
     # final solved sudoku
-    solved_board = solve_sudoku(board)
-    print('\nFinal solved sudoku: \n\n', np.matrix(solved_board))
-
+    sododku_solver.solve_sudoku()
+    print('\nFinal solved sudoku: \n\n', np.matrix(sododku_solver.get_board()))
 
     return
 
